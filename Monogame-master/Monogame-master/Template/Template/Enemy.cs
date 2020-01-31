d26d1086f6cf4a00517d99445e46717406a1a8cf
Elev <Elev@TEB127022>
2020-01-31 13:21:32 +0100

    försökte mitt bästa iaf. La till en klass för fienden

---
+++ Monogame-master/Monogame-master/Template/Template/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Template
+{
+    class Enemy
+    {
+        Texture2D enemy;
+        Random enemyspawn = new Random();
+        Vector2 enemyPos = new Vector2();
+
+        public Enemy(Texture2D enemy)
+        {
+            this.enemy = enemy;
+            enemyPos.X = enemyspawn.Next(0, 800);
+
+        }
+
+        public void Update()
+        {
+            enemyPos.Y--;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(enemy, new Rectangle((int)enemyPos.X, (int)enemyPos.Y, 30, 40), Color.White);
+        }
+    }
+
+    
+}
